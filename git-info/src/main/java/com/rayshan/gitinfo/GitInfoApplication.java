2d4e4dc47be183d8ad3a4d22907ae626d23af654
noushadbm <noushadbm@gmail.com>
2021-02-25 12:03:16 +0530

    first commit

---
+++ git-info/src/main/java/com/rayshan/gitinfo/GitInfoApplication.java
@@ -0,0 +1,13 @@
+package com.rayshan.gitinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitInfoApplication.class, args);
+	}
+
+}
---
+++ git-info/src/test/java/com/rayshan/gitinfo/GitInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayshan.gitinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
